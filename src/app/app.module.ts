559719cb8281fd63c1af56c0c1249e7422ab364e
Andferreiral <andrey123.ferreira@hotmail.com>
2018-06-01 22:30:35 -0300

    Aula 01 Ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ListaPaisesPageModule } from '../pages/lista-paises/lista-paises.module';
+import { DetalhePaisPageModule } from '../pages/detalhe-pais/detalhe-pais.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    ListaPaisesPageModule,
+    DetalhePaisPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
